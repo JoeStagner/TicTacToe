d3fa64b6709e5c6dff0889ddac09118399912d4a
Joe Stagner <Joe@Stagner.net>
2015-03-24 08:48:51 -0400

    Images

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 var turn = 0;
 var imageName="";
 
-var pictureSource;   // picture source
-var destinationType; // sets the format of returned value 
+var pictureSource;   
+var destinationType; 
 var counter = 0;
 document.addEventListener("deviceready",onDeviceReady,false);
 
@@ -34,8 +34,6 @@ function toggleBox(name) {
 			counter++;
 		}
 	}
-	
-	
 }
 
 function clearGame() {
@@ -45,30 +43,8 @@ function clearGame() {
 		var box = document.getElementById("box" + i);
 		box.src = "blank.png";
 	}	
-
-}
-
-function changePhoto(name) {
-	imageName = name;
-	//alert('Changing Photo');
-	// Take picture using device camera and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.DATA_URL });
 }
 
 function onFail(message) {
     alert('Failed because: ' + message);
 }
-
-function onPhotoDataSuccess(imageData) {
-      // Get image handle
-      //
-      var smallImage = document.getElementById(imageName);
-
-      // Unhide image elements
-      //
-      //smallImage.style.display = 'block';
-
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      smallImage.src = "data:image/jpeg;base64," + imageData;}
